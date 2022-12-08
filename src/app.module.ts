cef87c0dd2572d11cdda63dfcd9f6b515cad3a08
kinjo yuya <kinjo.bobchin@gmail.com>
2022-12-08 21:39:18 +0900

    feat: add gql federation settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: { cors: true },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'users', url: 'http://localhost:3001/graphql/users' },
+            { name: 'posts', url: 'http://localhost:3002/graphql/posts' },
+            { name: 'comments', url: 'http://localhost:3003/graphql/comments' },
+          ],
+        }),
+      },
+    }),
+  ],
 })
 export class AppModule {}
